72b6a0da98314199e4c510ac2fbf613920259655
Rakun <jnicolaybv@gmail.com>
2024-08-16 11:47:41 -0500

    rutes correction

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/register', [RegisterController::class, 'show']);
